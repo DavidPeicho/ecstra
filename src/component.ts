f5b15b5fb98f8830dd1c1a954b4d26547a46ee1d
David Peicho <david.peicho@gmail.com>
2021-01-30 19:29:39 +0100

    components: add property system

--- src/component.ts
+++ src/component.ts
@@ -1,16 +1,42 @@
+import { Property } from './property.js';
 import { Nullable } from './types';
 
-export class Component {
+export class Component<CompClass extends typeof Component = typeof Component> {
   public static readonly Name: Nullable<string> = null;
+  public static readonly Properties?: Properties;
   public readonly isComponent!: true;
 
   public constructor() {
     Object.defineProperty(this, 'isComponent', { value: true });
+
+    const properties = (this.constructor as CompClass).Properties;
+    for (const name in properties) {
+      this[name as keyof this] = properties[name].cloneDefault();
+    }
   }
 
-  public copy(source: this): this {
+  public copy(source: PropertiesOf<this>, useDefault = false): this {
+    const properties = (this.constructor as CompClass).Properties;
+    // @todo: inverse loop, should loop on source instead.
+    for (const name in properties) {
+      if (source.hasOwnProperty(name)) {
+        const prop = properties[name];
+        const value = source[name as keyof this];
+        this[name as keyof this] = prop.copy(value, this[name as keyof this]);
+      }
+    }
     return this;
   }
+
+  public clone(source: this): this {
+    const Class = (this.constructor as CompClass);
+    return (new Class() as this).copy(source);
+  }
+
+  public init(source: PropertiesOf<this>): this {
+    return this.copy(source, true);
+  }
+
 }
 
 export class TagComponent {
@@ -25,4 +51,11 @@ export class TagComponent {
 // @todo: up to one component per world on a dummy entity.
 export class SingletonComponent {}
 
+export type PropertiesOf<CompType extends Component> = {
+  [ K in keyof CompType ]: any;
+};
+
+export interface Properties {
+  [ key: string ]: Property<any>;
+}
 export type GenericComponent = Component | TagComponent;
---
+++ src/property.ts
@@ -0,0 +1,104 @@
+import { Constructor, Nullable } from "./types";
+
+export class Property<T> {
+  public static Name = 'BaseProperty';
+
+  protected _default: T;
+
+  public constructor(options: PropertyOptions<T>) {
+    this._default = options.default ?? options.typeDefault;
+  }
+
+  public copy(_: T, src: T): T {
+    return src;
+  }
+
+  public copyDefault(dest: T): T {
+    return this.copy(dest, this._default);
+  }
+
+  public cloneDefault(): T {
+    return this._default;
+  }
+}
+
+export class RefProp<T> extends Property<Nullable<T>> {
+
+  public constructor() {
+    super({ typeDefault: null });
+  }
+}
+
+export class BooleanProp extends Property<boolean> {
+
+  public constructor() {
+    super({ typeDefault: false });
+  }
+}
+
+export class NumberProp extends Property<number> {
+
+  public constructor() {
+    super({ typeDefault: 0 });
+  }
+}
+
+export class StringProp extends Property<string> {
+
+  public constructor() {
+    super({ typeDefault: '' });
+  }
+}
+
+export class ArrayProp<T> extends Property<T[]> {
+
+  public constructor(defaultValue?: T) {
+    super({ typeDefault: [] });
+  }
+
+  public copy(dst: T[], src: T[]): T[] {
+    dst.length = 0;
+    dst.push(...src);
+    return dst;
+  }
+
+  public cloneDefault(): T[] {
+    return this.copyDefault([]);
+  }
+}
+
+export class CopyProp<T extends CopyClonableType> extends Property<T> {
+  public static Name = 'CopyClonable';
+
+  public constructor(options: CopyClonableOptions<T>) {
+    super({ typeDefault: new options.type(), default: options.default });
+    // @todo: check that type is really copy/clonable in dev mode.
+  }
+
+  public copy(dst: T, src: T): T {
+    return dst.copy(src);
+  }
+
+  public copyDefault(dest: T): T {
+    return dest.copy(this._default);
+  }
+
+  public cloneDefault(): T {
+    return this._default.clone();
+  }
+}
+
+export interface PropertyOptions<T> {
+  typeDefault: T;
+  default?: T;
+}
+
+export interface CopyClonableOptions<T> {
+  type: Constructor<T>;
+  default?: T;
+}
+
+interface CopyClonableType {
+  copy(source: this): this;
+  clone(): this;
+}
