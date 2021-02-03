89e6f58dbf29349e236f5bcc697268faad133c4f
David Peicho <david.peicho@gmail.com>
2021-02-03 23:37:43 +0100

    constant: attempt at cross platform NODE_ENV for debug

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
-// @todo: make that work for Nde.js and browser
 export const process = {
-  env: { NODE_ENV: 'production' }
+  env: {
+    NODE_ENV: 'production'
+  }
 };
+const globalProcess = (globalThis as any).process;
+if (globalProcess && globalProcess.env && globalProcess.env.NODE_ENV != '') {
+  process.env.NODE_ENV = (globalThis as any).process.env.NODE_ENV;
+}
