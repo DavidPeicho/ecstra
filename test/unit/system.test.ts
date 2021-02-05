345f871ae6678fbfff7f5d36cb39ffa00f10a3ab
David Peicho <david.peicho@gmail.com>
2021-02-05 01:14:49 +0100

    pretty

--- test/unit/system.test.ts
+++ test/unit/system.test.ts
@@ -13,18 +13,18 @@ test('SystemGroup > topological sorting', (t) => {
     execute() {}
   }
 
-  @after([ SystemC ])
+  @after([SystemC])
   class SystemD extends System {
     execute() {}
   }
 
-  @after([ SystemC ])
-  @before([ SystemD ])
+  @after([SystemC])
+  @before([SystemD])
   class SystemA extends System {
     execute() {}
   }
 
-  @before([ SystemA ])
+  @before([SystemA])
   class SystemB extends System {
     execute() {}
   }
@@ -37,9 +37,9 @@ test('SystemGroup > topological sorting', (t) => {
   group.add(b);
   group.add(c);
   group.add(d);
-  t.deepEqual(group['_systems'], [ a, b, c, d ]);
+  t.deepEqual(group['_systems'], [a, b, c, d]);
   group.sort();
-  t.deepEqual(group['_systems'], [ c, b, a, d ]);
+  t.deepEqual(group['_systems'], [c, b, a, d]);
 });
 
 test('SystemGroup > topological sorting cycle', (t) => {
@@ -50,13 +50,13 @@ test('SystemGroup > topological sorting cycle', (t) => {
     execute() {}
   }
 
-  @after([ SystemC ])
+  @after([SystemC])
   class SystemD extends System {
     execute() {}
   }
 
-  @after([ SystemD ])
-  @before([ SystemC ])
+  @after([SystemD])
+  @before([SystemC])
   class SystemA extends System {
     execute() {}
   }
@@ -66,39 +66,33 @@ test('SystemGroup > topological sorting cycle', (t) => {
   group.add(c);
   group.add(a);
   group.add(d);
-  t.deepEqual(group['_systems'], [ c, a, d ]);
+  t.deepEqual(group['_systems'], [c, a, d]);
   group.sort();
   // In the case of a cycle, the final ordering will be dependant on the
   // insert order.
-  t.deepEqual(group['_systems'], [ c, d, a ]);
+  t.deepEqual(group['_systems'], [c, d, a]);
 });
 
-test('SystemGroup > topological sorting + custom order', (t) => {
+test('SystemGroup > ordering', (t) => {
   const world = new World();
   const group = new SystemGroup(world);
 
-  class SystemC extends System {
-    execute() {}
-  }
-
-  @after([ SystemC ])
-  class SystemD extends System {
-    execute() {}
-  }
-
-  @after([ SystemD ])
   class SystemA extends System {
     execute() {}
   }
-  const a = new SystemA(group, {});
-  const c = new SystemC(group, {});
-  const d = new SystemD(group, {});
+  class SystemB extends System {
+    execute() {}
+  }
+  class SystemC extends System {
+    execute() {}
+  }
+  const a = new SystemA(group, { order: -100 });
+  const c = new SystemC(group, { order: 1 });
+  const b = new SystemB(group, { order: 200 });
+  group.add(b);
   group.add(a);
-  group.add(d);
   group.add(c);
-  t.deepEqual(group['_systems'], [ c, a, d ]);
+  t.deepEqual(group['_systems'], [b, a, c]);
   group.sort();
-  // In the case of a cycle, the final ordering will be dependant on the
-  // insert order.
-  t.deepEqual(group['_systems'], [ c, d, a ]);
+  t.deepEqual(group['_systems'], [a, c, b]);
 });
