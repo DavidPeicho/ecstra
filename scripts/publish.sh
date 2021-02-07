8e6a4aaa8e856af4cee807acd28e74347ef7b527
David Peicho <david.peicho@gmail.com>
2021-02-07 18:58:42 +0100

    scripts: publish: update script to build and publish dist

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
+rm -rf dist
+
+echo "[ PUBLISH ]: Linting package..."
+npm run lint
+
+echo "[ PUBLISH ]: Running tests..."
+npm run test
+
+echo "[ PUBLISH ]: Generating build files..."
 npm run build
-# npm publish ./lib
+
+# Switch to public profile to ensure publisher is using public npmrc config
+npmrc public-profile
+
+npm publish ./dist
